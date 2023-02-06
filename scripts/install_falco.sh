b8d62bbcb1fc9c1daf0e868c1da96401e6fa5ae0
Samuel Vijaykumar M <mad.vijay@gmail.com>
2023-02-06 09:51:52 +0530

    disable falco service

--- scripts/install_falco.sh
+++ scripts/install_falco.sh
@@ -8,3 +8,4 @@ sudo rpm --import https://falco.org/repo/falcosecurity-3672BA8F.asc
 sudo curl -s -o /etc/yum.repos.d/falcosecurity.repo https://falco.org/repo/falcosecurity-rpm.repo
 sudo yum install -y kernel-devel-$(uname -r)
 sudo yum install -y falco
+sudo systemctl disable falco
