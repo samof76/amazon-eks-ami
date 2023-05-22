29c9ae60d0236f676f42086fb1671a636621b0ae
M Samuel Vijaykumar <mad.vijay@gmail.com>
2023-05-22 16:55:35 +0530

    Adding sed

--- scripts/disable_auto_upgrade.sh
+++ scripts/disable_auto_upgrade.sh
@@ -3,4 +3,4 @@
 # This script is used to disable auto-upgrade on the Amazon Linux 2 AMI.
 # It is run as part of the AMI build process.
 
-sed -i 's/^repo_upgrade: .*/repo_upgrade: none/' /etc/cloud/cloud.cfg
+sudo sed -i 's/^repo_upgrade: .*/repo_upgrade: none/' /etc/cloud/cloud.cfg
